8f86e640f2b764cfdc8d8f95da1fc77114e60a8e
JackProIsFake <downloadgames310806@gmail.com>
2024-06-27 10:32:36 -0300

    Calculadora de Média

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+print ('Calculadora para a nota média')
+
+aluno = input('Digite o nome do aluno:')
+
+print (aluno)
+
+print ('Digite as notas')
+
+nota_1 = float(input('1º Nota:'))
+nota_2 = float(input('2º Nota:'))
+nota_3 = float(input('3º Nota:'))
+nota_4 = float(input('4º Nota:'))
+
+notas = (nota_1 + nota_2 + nota_3 + nota_4)
+media = (notas/4)
+
+if media >= 6: 
+    print ('Parabéns, você passou.')
+else:
+    print ('Você reprovou, estude mais na próxima.')
+
